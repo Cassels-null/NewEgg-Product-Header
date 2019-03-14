d9e1fa25d12cad7d616e56b11be0b20bd0cea364
Cassels-null <42cassels@gmail.com>
2019-03-14 17:02:22 -0400

    bugfix

--- client/config.js
+++ client/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  aws: "http://ec2-54-87-165-69.compute-1.amazonaws.com"
+  aws: "ec2-54-87-165-69.compute-1.amazonaws.com"
   // aws: "localhost:3010"
   // aws: "127.0.0.1:3010"
 }
